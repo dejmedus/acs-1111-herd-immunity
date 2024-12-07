83afbf28687b1f44c8c6da4ca454b2471f06ee22
Julia <email96123@gmail.com>
2024-12-07 12:59:36 -0700

    add virus attributes

--- virus.py
+++ virus.py
@@ -3,13 +3,13 @@ class Virus(object):
     def __init__(self, name, repro_rate, mortality_rate):
         # Define the attributes of your your virus
         self.name = name
-        # TODO Define the other attributes of Virus
-        pass
+        self.repro_rate = repro_rate
+        self.mortality_rate = mortality_rate
 
 
 # Test this class
 if __name__ == "__main__":
-    # Test your virus class by making an instance and confirming 
+    # Test your virus class by making an instance and confirming
     # it has the attributes you defined
     virus = Virus("HIV", 0.8, 0.3)
     assert virus.name == "HIV"
